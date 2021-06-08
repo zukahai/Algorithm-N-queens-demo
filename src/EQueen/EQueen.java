859cd7b616615802f31227ca485556a9b307a723
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-08 19:25:42 +0700

    Done

--- src/EQueen/EQueen.java
+++ src/EQueen/EQueen.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,6 +14,8 @@ import java.util.Stack;
 import java.util.Vector;
 
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
@@ -33,12 +36,20 @@ public class EQueen extends JFrame{
 	TextArea tf;
 	JButton bt[][] = new JButton[200][200];
 	int Q[][] = new int[200][200];
+	int cl[][] = new int[200][200];
 	int N = 6;
 	int delay = 0;
+	int k = 1, K = 100;
+	
+	public int HaiZuka(int delay) {
+		return 150 / delay;
+	}
+	
 	public EQueen(int N, int delay) {
 		super("Algorithms eight queens demo - HaiZuka");
 		this.N = N;
 		this.delay = delay;
+		K = k = HaiZuka((int)Math.sqrt(delay));
 		cn = init();
 		timer.start();
 	}
@@ -72,56 +83,86 @@ public class EQueen extends JFrame{
 		
 		this.setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-//		this.setResizable(false);
+		this.setResizable(false);
 		this.setSize(1100, 700);
 		this.setLocationRelativeTo(null);
+		setIcon();
 		timer = new Timer(delay, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Vector v = new Vector();
-				if (!st.empty())
-					v = st.peek();
-				int I = (int)v.get(0);
-				int J = (int)v.get(1) + 1;
-				if (J > 1) {
-					bt[I][J - 1].setBackground(Color.white);
-					J--;
-					if (Q[I][J] == 1) {
-						c[J] = c1[I - J + N] = c2[I + J - 2] = true;
-						Q[I][J] = 0;
-					}
-					J++;
-				}
-				if (J == N + 1 ) {
-					st.pop();
-					if (st.empty())
-						timer.stop();
-				} else {
-					v.set(1, J);
-					bt[I][J].setBackground(Color.green);
-					if (check(I, J)) {
-						Q[I][J] = 1;
-						c[J] = c1[I - J + N] = c2[I + J - 2] = false;
-						Vector vv = new Vector();
-						vv.add(I + 1);
-						vv.add(0);
-						st.pop();
-						st.push(v);
-						if (I == N) {
-							Stack<Vector<Integer>> ST = st;
-							tf.setText(tf.getText() + (count++) + ": " + getChess(ST) + "\n");
-//							timer.stop();
-						} else {
-							st.push(vv);
+				if (k == K) {
+					Vector v = new Vector();
+					if (!st.empty())
+						v = st.peek();
+					int I = (int)v.get(0);
+					int J = (int)v.get(1) + 1;
+					if (J > 1) {
+						bt[I][J - 1].setBackground(Color.white);
+						cl[I][J - 1] = 0;
+						seticonSquar(I, J - 1);
+						J--;
+						if (Q[I][J] == 1) {
+							c[J] = c1[I - J + N] = c2[I + J - 2] = true;
+							Q[I][J] = 0;
+							cl[I][J] = 0;
 						}
+						J++;
+					}
+					if (J == N + 1 ) {
+						st.pop();
+						if (st.empty())
+							timer.stop();
 					} else {
-						bt[I][J].setBackground(Color.yellow);
+						v.set(1, J);
+						bt[I][J].setBackground(Color.green);
+						if (check(I, J)) {
+							Q[I][J] = cl[I][J] = 1;
+							seticonSquar(I, J);
+							c[J] = c1[I - J + N] = c2[I + J - 2] = false;
+							Vector vv = new Vector();
+							vv.add(I + 1);
+							vv.add(0);
+							st.pop();
+							st.push(v);
+							if (I == N) {
+								Stack<Vector<Integer>> ST = st;
+								tf.setText(tf.getText() + (count++) + ": " + getChess(ST) + "\n");
+								k = 0;
+//								timer.stop();
+							} else {
+								st.push(vv);
+							}
+						} else {
+							cl[I][J] = 2;
+							seticonSquar(I, J);
+						}
 					}
 				}
-//				System.out.println(st);
+				else k++;
 			}
 		});
 		
 		return cn;
+	}
+	
+	public void setIcon() {
+		for (int i = 1; i <= N; i++)
+			for (int j = 1; j <= N; j++) {
+				seticonSquar(i, j);
+			}
+	}
+	
+	public void seticonSquar(int i, int j) {
+		String name = "";
+		name = name + cl[i][j] +(i + j) % 2;
+		bt[i][j].setIcon(getIcon(name));
+	}
+	
+	private Icon getIcon(String name) {
+		int size = 700 / N;
+		int width = size, height = size;
+		Image image = new ImageIcon(getClass().getResource("/Icons/" + name + ".png")).getImage();
+		Icon icon = new ImageIcon(image.getScaledInstance(width, height, image.SCALE_SMOOTH));
+		return icon;
 	}
 	
 	public String getChess(Stack<Vector<Integer>> stt) {
@@ -147,6 +188,6 @@ public class EQueen extends JFrame{
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new EQueen(4, 1);
+		new EQueen(5, 1);
 	}
 }

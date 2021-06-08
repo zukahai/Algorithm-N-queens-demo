4ee78c7eef806f158b4b1e8a6015d43a446940cf
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-06-08 16:18:51 +0700

    Gan xong

--- src/EQueen/EQueen.java
+++ src/EQueen/EQueen.java
@@ -22,12 +22,15 @@ import javax.swing.Timer;
 public class EQueen extends JFrame{
 	Container cn;
 	Stack<Vector<Integer>> st = new Stack<>();
-	boolean c1[] = new boolean[20];
-	boolean c2[] = new boolean[20];
-	boolean c[] = new boolean[20];
-	boolean h[] = new boolean[20];
+	boolean c1[] = new boolean[2000];
+	boolean c2[] = new boolean[2000];
+	boolean c[] = new boolean[2000];
 	Timer timer;
-	JButton bt[][] = new JButton[9][9];
+	int count = 1;
+	JButton bt[][] = new JButton[100][100];
+	int Q[][] = new int[100][100];
+	int N = 4;
+	int delay = 1000;
 	public EQueen() {
 		super("Algorithms eight queens demo - HaiZuka");
 		cn = init();
@@ -40,16 +43,17 @@ public class EQueen extends JFrame{
 		v.add(0);
 		st.push(v);
 		for (int i = 0; i < 20; i++)
-			c1[i] = c2[i] = h[i] = c[i] = true;
+			c1[i] = c2[i] = c[i] = true;
 		JPanel pn = new JPanel();
-		pn.setLayout(new GridLayout(8, 8));
+		pn.setLayout(new GridLayout(N, N));
 		
-		for (int i = 1; i <= 8; i ++)
-			for (int j = 1; j <= 8; j++) {
+		for (int i = 1; i <= N; i ++)
+			for (int j = 1; j <= N; j++) {
 				bt[i][j] = new JButton();
 //				bt[i][j].setFont(new Font("Britannic Bold", 1, 25));
-//				bt[i][j].setBackground(Color.black);
+				bt[i][j].setBackground(Color.white);
 				bt[i][j].setForeground(Color.white);
+				Q[i][j] = 0;
 				pn.add(bt[i][j]);
 			}
 		
@@ -61,38 +65,45 @@ public class EQueen extends JFrame{
 		this.setResizable(false);
 		this.setSize(500, 500);
 		this.setLocationRelativeTo(null);
-		timer = new Timer(1, new ActionListener() {
+		timer = new Timer(delay, new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Vector v = st.peek();
 				int I = (int)v.get(0);
 				int J = (int)v.get(1) + 1;
-				if (J > 1)
+				if (J > 1) {
 					bt[I][J - 1].setBackground(Color.white);
-				if (J == 9 ) {
 					J--;
-					h[I] = c[J] = c1[I - J + 7] = c2[I + J - 2] = true;
+					if (Q[I][J] == 1) {
+						c[J] = c1[I - J + N] = c2[I + J - 2] = true;
+						Q[I][J] = 0;
+					}
+					J++;
+				}
+				if (J == N + 1 ) {
 					st.pop();
-					v = st.peek();
-					I = (int)v.get(0);
-					J = (int)v.get(1);
-					h[I] = c[J] = c1[I - J + 7] = c2[I + J - 2] = true;
+					if (st.empty())
+						timer.stop();
 				} else {
 					v.set(1, J);
 					bt[I][J].setBackground(Color.green);
 					if (check(I, J)) {
-						h[I] = c[J] = c1[I - J + 7] = c2[I + J - 2] = false;
+						Q[I][J] = 1;
+//						System.out.println((I - J + 7) + " : " + c1[I - J + 7]);
+						c[J] = c1[I - J + N] = c2[I + J - 2] = false;
+//						System.out.println((I - J + 7) + " : " + c1[I - J + 7]);
 						Vector vv = new Vector();
 						vv.add(I + 1);
 						vv.add(0);
 						st.pop();
 						st.push(v);
-						if (I == 8) {
-							System.out.println(st);
+						if (I == N) {
+							System.out.println((count++) +": " + st);
+//							timer.stop();
 						} else {
 							st.push(vv);
 						}
 					} else {
-						bt[I][J].setBackground(Color.white);
+						bt[I][J].setBackground(Color.yellow);
 					}
 				}
 			}
@@ -102,7 +113,7 @@ public class EQueen extends JFrame{
 	}
 	
 	public boolean check(int i, int j) {
-		if (!h[i] || !c[j] || !c1[i - j + 7] || !c2[i + j - 2])
+		if (!c[j] || !c1[i - j + N] || !c2[i + j - 2])
 			return false;
 		return true;
 	}
